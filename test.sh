048aba77a16781db5e00c9d695a41a9e448af8b6
weaming <garden.yuen@gmail.com>
2026-08-02 21:02:39 +0800

    v34.8.0: terminal, code, web, webcode

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e "正常体: a g i l o 0 O | const val = 0; 中英文 2:1 黄金对齐" && \
+echo -e "\033[1m纯粗体: a g i l o 0 O | const val = 0; 汉字加粗清晰测试\033[0m" && \
+echo -e "\033[3m纯斜体: a g i l o 0 O | const val = 0; 手写弧度跨族调用\033[0m" && \
+echo -e "\033[1;3m粗斜体: a g i l o 0 O | const val = 0; 终极复合无缝渲染\033[0m"
+
